dbacd1ed039110876dcea54ad0e53185d4895c2c
KANG Jian <jkangac@connect.ust.hk>
2019-02-28 18:28:03 +0800

    test

--- image_proc_test.py
+++ image_proc_test.py
@@ -14,5 +14,20 @@ plt.xticks([]), plt.yticks([])
 plt.plot([200,300,400],[100,200,300],'c',linewidth=2)
 plt.show()
 """
- 
-cap = cv2.VideoCapture(0);
+"""
+cap = cv2.VideoCapture("test.mp4")
+while(True):
+	ret, frame = cap.read()
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	cv2.imshow('frame', gray)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+cap.release
+cv2.destroyAllWindows
+"""
+img = cv2.imread('girl.jpeg', cv2.IMREAD_COLOR)
+img[55, 55] = [255, 255, 255]
+px = img[55, 55]
+print(px)
+px = img[100:150, 100:150]
+print(px)
